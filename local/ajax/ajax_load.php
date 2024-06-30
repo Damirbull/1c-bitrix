5a7547f583facfc8b4c5136526cca884470464ea
Damirbull <125370994+Damirbull@users.noreply.github.com>
2024-06-30 21:47:44 +0500

    Update ajax_load.php

--- local/ajax/ajax_load.php
+++ local/ajax/ajax_load.php
@@ -1,35 +1,35 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
-// Подключаем модуль инфоблоков
+
 use Bitrix\Main\Loader;
 Loader::includeModule('iblock');
 
-// Функция для импорта данных из JSON и добавления в инфоблок
+
 function importJsonData($iblockId, $numRecords) {
-    // URL для получения JSON данных
+    
     $url = 'https://jsonplaceholder.typicode.com/posts';
 
-    // Получаем данные
+    
     $jsonData = file_get_contents($url);
     if ($jsonData === false) {
         die('Не удалось получить данные.');
     }
 
-    // Парсим JSON данные в массив
+    
     $data = json_decode($jsonData, true);
     if ($data === null) {
         die('Ошибка при парсинге JSON данных.');
     }
 
-    // Ограничиваем количество записей
+    
     $data = array_slice($data, 0, $numRecords);
 
-    // Добавляем записи в инфоблок
+    
     foreach ($data as $record) {
         $el = new CIBlockElement;
 
-        // Параметры нового элемента
+        
         $arLoadProductArray = array(
             "IBLOCK_ID"      => $iblockId,
             "NAME"           => "Next level opportunities are creating dynamic worlds", 
@@ -43,7 +43,7 @@ function importJsonData($iblockId, $numRecords) {
             ),
         );
 
-        // Добавляем элемент в инфоблок
+        
         if (!$el->Add($arLoadProductArray)) {
             echo 'Ошибка добавления записи: ' . $el->LAST_ERROR . '<br>';
         } else {
@@ -52,11 +52,11 @@ function importJsonData($iblockId, $numRecords) {
     }
 }
 
-// Проверяем, является ли запрос Ajax
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'importData') {
-    // Вызываем функцию импорта данных
-    importJsonData(4, 3); // Пример: Добавит 3 записи в инфоблок с ID 5
-    exit; // Выходим после обработки Ajax запроса
+
+    importJsonData(4, 3); // Пример: Добавит 3 записи в инфоблок 4
+    exit; 
 }
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");
 ?>
